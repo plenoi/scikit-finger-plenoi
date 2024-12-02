f7e14527b11be0d437e6de7500d75a8ae0bd0e20
plenoi <45334571+plenoi@users.noreply.github.com>
2024-12-02 12:55:49 +0700

    Update topological_torsion.py

--- skfp/fingerprints/topological_torsion.py
+++ skfp/fingerprints/topological_torsion.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 import numpy as np
 from rdkit.Chem import Mol
 from scipy.sparse import csr_array
-from sklearn.utils._param_validation import Interval
+
 
 from skfp.bases import BaseFingerprintTransformer
 from skfp.utils import ensure_mols
@@ -111,14 +111,6 @@ class TopologicalTorsionFingerprint(BaseFingerprintTransformer):
 
     """
 
-    _parameter_constraints: dict = {
-        **BaseFingerprintTransformer._parameter_constraints,
-        "fp_size": [Interval(Integral, 1, None, closed="left")],
-        "torsion_atom_count": [Interval(Integral, 2, None, closed="left")],
-        "use_pharmacophoric_invariants": ["boolean"],
-        "include_chirality": ["boolean"],
-        "count_simulation": ["boolean"],
-    }
 
     def __init__(
         self,
