0b3303bfa0795f083e53f78cbbd8850b645e7eb5
plenoi <45334571+plenoi@users.noreply.github.com>
2024-12-02 12:55:27 +0700

    Update secfp.py

--- skfp/fingerprints/secfp.py
+++ skfp/fingerprints/secfp.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 import numpy as np
 from rdkit.Chem import Mol
 from scipy.sparse import csr_array
-from sklearn.utils._param_validation import Interval, InvalidParameterError
+
 
 from skfp.bases import BaseFingerprintTransformer
 from skfp.utils.validators import ensure_smiles
@@ -106,15 +106,6 @@ class SECFPFingerprint(BaseFingerprintTransformer):
            [0, 0, 0, ..., 0, 0, 0]], dtype=uint8)
     """
 
-    _parameter_constraints: dict = {
-        **BaseFingerprintTransformer._parameter_constraints,
-        "fp_size": [Interval(Integral, 1, None, closed="left")],
-        "radius": [Interval(Integral, 1, None, closed="left")],
-        "min_radius": [Interval(Integral, 1, None, closed="left")],
-        "sssr_rings": ["boolean"],
-        "isomeric_smiles": ["boolean"],
-        "kekulize": ["boolean"],
-    }
 
     def __init__(
         self,
@@ -145,12 +136,7 @@ class SECFPFingerprint(BaseFingerprintTransformer):
 
     def _validate_params(self) -> None:
         super()._validate_params()
-        if self.radius < self.min_radius:
-            raise InvalidParameterError(
-                f"The radius parameter of {self.__class__.__name__} must be "
-                f"greater or equal to min_radius, got: "
-                f"min_radius={self.min_radius}, radius={self.radius}"
-            )
+
 
     def _calculate_fingerprint(
         self, X: Sequence[Union[str, Mol]]
